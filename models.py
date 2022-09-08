79dc3c70a90670b23818e01ee992d0b8582d2e32
IceMerman <jsjuanes51@gmail.com>
2022-09-08 00:04:19 -0500

    TPathOP_struct_users

--- models.py
+++ models.py
@@ -10,21 +10,21 @@ from pydantic import BaseModel, EmailStr, Field, SecretStr, validator
 
 # User models
 class UserBase(BaseModel):
-    user_id: UUID = Field()
-    email: EmailStr = Field(example='jhon.snow@winterfall.com')    
+    user_id: UUID = Field(...)
+    email: EmailStr = Field(..., example='jhon.snow@winterfall.com')    
 class UserLogin(UserBase):
-    password: SecretStr = Field(
+    password: str = Field(
         ...,
         min_length=8,
         max_length=32,
         regex='^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#_-])[A-Za-z\d@$!%*?&#_-]{8,32}$',
         description='Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number and one special character.')
 class User(UserBase):
-    first_name: str = Field(min_length=3, max_length=50, example='Jhon')
-    last_name: str = Field(min_length=3, max_length=50, example='Snow')
+    first_name: str = Field(..., min_length=3, max_length=50, example='Jhon')
+    last_name: str = Field(..., min_length=3, max_length=50, example='Snow')
     birth_date: Optional[date] = Field(None, example=date.today())
     
-    @validator(birth_date)
+    @validator('birth_date')
     def is_adult(cls, v: date):
         today = date.today()
         delta = today - v
@@ -35,7 +35,7 @@ class User(UserBase):
 # Tweet models
 class Tweet(BaseModel):
     tweet_id: UUID = Field()
-    content: str = Field(min_length=1, max_length=300, example='Example tweet')
-    created_at: date = Field(default=datetime.utcnow().replace(tzinfo=tz.tzutc()))
-    updated_at: Optional[date] = Field(None, default=datetime.utcnow().replace(tzinfo=tz.tzutc()))
-    by: User = Field()
+    content: str = Field(..., min_length=1, max_length=300, example='Example tweet')
+    created_at: date = Field(default=datetime.utcnow()) #.replace(tzinfo=tz.tzutc())
+    updated_at: Optional[date] = Field(default=datetime.utcnow()) #.replace(tzinfo=tz.tzutc())
+    by: User = Field(...)
--- twitter.py
+++ twitter.py
@@ -1,13 +1,152 @@
 #Python
 
 #Custom
-import models
+from typing import Dict, List, NoReturn
+from models import Tweet, User
 
 #Fast api
-from fastapi import FastAPI
+from fastapi import FastAPI, Path
+from fastapi import status
 
 app = FastAPI()
 
+# Path operations
+
 @app.get(path='/')
-def home():
+def home() -> Dict[str, str]:
     return {'Twitter API': 'Active'}
+
+## Users
+
+### Create a user
+@app.post(
+    path='/auth/signup',
+    response_model=User,
+    status_code=status.HTTP_201_CREATED,
+    summary='Register a user',
+    tags=['Auth','Users']
+)
+def signup() -> User:
+    pass
+
+### Authentica the user
+@app.post(
+    path='/auth/login',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Login with user',
+    tags=['Auth', 'Users']
+)
+def login() -> User:
+    pass
+
+### Get all users
+@app.get(
+    path='/users',
+    response_model=List[User],
+    status_code=status.HTTP_200_OK,
+    summary='Show all users',
+    tags=['Users']
+)
+def list_users() -> List[User]:
+    pass
+
+### Get a user
+@app.get(
+    path='/users/{user_id}',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Get a users by id',
+    tags=['Users']
+)
+def retrieve_user(
+    user_id: int = Path(gt=0, title='User ID', description='The ID of the user to retrieve', example=1)
+    ) -> User:
+    pass
+
+### Update a user
+@app.put(
+    path='/users/{user_id}',
+    response_model=User,
+    status_code=status.HTTP_200_OK,
+    summary='Update a user',
+    tags=['Users']
+)
+def update_user(
+    user_id: int = Path(gt=0, title='User ID', description='The ID of the user to update', example=1)
+    ) -> User:
+    pass
+
+### Delete a user
+@app.delete(
+    path='/users/{user_id}',
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary='Delete a user',
+    tags=['Users']
+)
+def delete_user(
+    user_id: int = Path(gt=0, title='User ID', description='The ID of the user to delete', example=1)
+    ) -> NoReturn:
+    pass
+
+## Tweets
+
+### Create a tweet
+@app.post(
+    path='/tweet',
+    response_model=Tweet,
+    status_code=status.HTTP_201_CREATED,
+    summary='Create a new tweet',
+    tags=['Tweets']
+)
+def create_tweet() -> Tweet:
+    pass
+
+### Show all tweets
+@app.get(
+    path='/tweet',
+    response_model=List[Tweet],
+    status_code=status.HTTP_200_OK,
+    summary='List all tweets',
+    tags=['Tweets']
+)
+def list_tweet() -> List[Tweet]:
+    pass
+
+### Show a tweet
+@app.get(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_200_OK,
+    summary='Show a tweet',
+    tags=['Tweets']
+)
+def get_tweet(
+    tweet_id: int = Path(gt=0, title='Tweet ID', description='Retrieve a tweet', example=1)
+    ) -> Tweet:
+    pass
+
+### Update a tweet
+@app.put(
+    path='/tweet/{tweet_id}',
+    response_model=Tweet,
+    status_code=status.HTTP_202_ACCEPTED,
+    summary='Update a tweet',
+    tags=['Tweets']
+)
+def update_tweet(
+    tweet_id: int = Path(gt=0, title='Tweet ID', description='Retrieve a tweet', example=1)
+    ) -> Tweet:
+    pass
+
+### Delete a tweet
+@app.delete(
+    path='/tweet/{tweet_id}',
+    status_code=status.HTTP_204_NO_CONTENT,
+    summary='Delete a tweet',
+    tags=['Tweets']
+)
+def delete_tweet(
+    tweet_id: int = Path(gt=0, title='Tweet ID', description='Retrieve a tweet', example=1)
+    ) -> NoReturn:
+    pass
